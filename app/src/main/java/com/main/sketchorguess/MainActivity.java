51501dd5a71a82766ea173df26578e0120192491
ben255 <ben255@users.noreply.github.com>
2020-09-01 01:54:37 +0200

    Updated get request
    asdf

--- app/src/main/java/com/main/sketchorguess/MainActivity.java
+++ app/src/main/java/com/main/sketchorguess/MainActivity.java
@@ -1,33 +1,76 @@
 package com.main.sketchorguess;
 
 import android.content.Intent;
+import android.net.Network;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.Cache;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button playButton;
     private EditText loginText;
 
+    final TextView textView = (TextView) findViewById(R.id.text);
+// ...
+
+    // Instantiate the cache
+    DiskBasedCache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+    // Set up the network to use HttpURLConnection as the HTTP client.
+    BasicNetwork network = new BasicNetwork(new HurlStack());
+
+    // Instantiate the RequestQueue.
+    RequestQueue queue = Volley.newRequestQueue(this);
+    String url ="10.0.0.9:3000";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         loginText = (EditText) findViewById(R.id.login_edit_text);
 
-        playButton = (Button) findViewById(R.id.play_button);
+                playButton = (Button) findViewById(R.id.play_button);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
     }
+
 }
