7935c5d29b9c19ee836a77956dd4d7437b7f02ea
ben255 <ben255@users.noreply.github.com>
2020-09-01 01:09:41 +0200

    asd
    asd

--- app/src/main/java/com/main/sketchorguess/MainActivity.java
+++ app/src/main/java/com/main/sketchorguess/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
     }
